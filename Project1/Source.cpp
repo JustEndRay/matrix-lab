d85808bb9378b2dda3f7eeeb1438403b6d657ac3
Andrew Pastushuk <andreypastushuk@gmail.com>
2020-11-23 15:11:20 +0200

    version 1.2

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,42 +1,34 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-void Create(int** a, const int n, const int m);
-void Print(int** a, const int n, const int m);
-int is_symmetric(int** a, int n, int in, int j, int count);
+void create(int** a, const int n, const int m);
+void print(int** a, const int n, const int m);
+int symmetric(int** a, int n, int in, int j, int count);
 int main()
 {
-    srand((unsigned)time(NULL));
-    int n;
-    int m;
-    cout << "n = ";
-    cin >> n;
-    cout << "m = ";
-    cin >> m;
-    if (n == m)
-    {
-
-    }
-    else
-    {
-        cout << "matrix is not simetric" << endl;
-        return 0;
-    }
-
+    int n, m;
+    cout << "n = ";cin >> n;
+    cout << "m = ";cin >> m;
     int** a = new int* [n];
     for (int i = 0; i < n; i++)
         a[i] = new int[m];
-    Create(a, n, m);
-    Print(a, n, m);
-    int f = is_symmetric(a, n, n-1, 0, 0);
-    if (f == (n * n - n)) { cout << "simetric " <<f<< endl; }
-    else { cout << "not simetric " <<f<< endl; }
+    create(a, n, m);
+    print(a, n, m);
+    if (n != m)
+    {
+        cout << "not simetric" << endl;
+        return 0;
+    }
+
+    int f = symmetric(a, n, n-1, 0, 0);
+    if (f == (n * n - n)) { cout << "simetric " << endl; }
+    else { cout << "not simetric " << endl; }
     for (int i = 0; i < n; i++)
         delete[] a[i];
     delete[] a;
     return 0;
 }
-void Create(int** a, const int n, const int m)
+void create(int** a, const int n, const int m)
 {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
@@ -44,7 +36,7 @@ void Create(int** a, const int n, const int m)
             cout << "a[" << i << "][" << j<<"] = "; cin >> a[i][j];
         }
 }
-void Print(int** a, const int n, const int m)
+void print(int** a, const int n, const int m)
 {
     cout << endl;
     for (int i = 0; i < n; i++)
@@ -55,16 +47,16 @@ void Print(int** a, const int n, const int m)
     }
     cout << endl;
 }
-int is_symmetric(int** a, int n,int in, int j, int count)
+int symmetric(int** a, int n,int in, int j, int count)
 {
     if (in>-1)
     {
         if (j <n)
         {
-            if (a[in][j]==a[j][in]) { is_symmetric(a, n, in, j + 1, count + 1); }
-            else { is_symmetric(a, n, in, j + 1, count); }
+            if (a[in][j]==a[j][in]) { symmetric(a, n, in, j + 1, count + 1); }
+            else { symmetric(a, n, in, j + 1, count); }
         }
-        is_symmetric(a, n, in-1, j, count);
+        symmetric(a, n, in-1, j, count);
     }
     return count;
 }
